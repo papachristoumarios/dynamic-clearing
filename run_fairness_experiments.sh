f87a796a1daa351ade1b446281cc85ec4f7c0d74
Marios Papachristou <papachristoumarios@gmail.com>
2024-06-24 19:38:23 -0400

    Minor changes

---
+++ run_fairness_experiments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 main.py --name synthetic --num_iters 50 --method fractional --gini 0.5 --gini_type sgc -L -1 -B 50 &>synthetic_fairness_sgc &
+python3 main.py --name tlc --num_iters 1 --method fractional --gini 0.5 --gini_type sgc -L -1 -B 100 &>tlc_fairness_sgc &
+python3 main.py --name venmo --num_iters 50 --method fractional --gini 0.5 --gini_type sgc -L -1 -B 100 &>venmo_fairness_sgc & 
+# python3 main.py --name safegraph --num_iters 1 --method fractional --gini 0.5 --gini_type sgc -L -1 -B 500000 &>safegraph_fairness_sgc & 
+
+python3 main.py --name synthetic --num_iters 50 --method fractional --gini 0.5 --gini_type standard -L -1 -B 50 &>synthetic_fairness_gc &
+python3 main.py --name tlc --num_iters 1 --method fractional --gini 0.5 --gini_type standard -L -1 -B 100 &>tlc_fairness_gc &
+python3 main.py --name venmo --num_iters 50 --method fractional --gini 0.5 --gini_type standard -L -1 -B 100 &>venmo_fairness_gc & 
+# python3 main.py --name safegraph --num_iters 1 --method fractional --gini 0.5 --gini_type standard -L -1 -B 500000 &>safegraph_fairness_gc & 
